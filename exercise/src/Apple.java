f7a636f01184d6654e9102692b711114090573cc
mao381332619 <381332619@qq.com>
2021-01-02 20:55:49 +0800

    Initial commit

---
+++ exercise/src/Apple.java
@@ -0,0 +1,14 @@
+/**
+ * @author maoyuanpeng
+ * @date 2021/1/2 - 19:48
+ */
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+    public void howToEat(int b){
+        int num1=10;
+        int num2 =13;
+        System.out.println("num2 = " + num2);
+    }
+}
